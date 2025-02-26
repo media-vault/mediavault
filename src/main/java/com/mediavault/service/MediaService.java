a86bb2c3365023a49cccff40838fe7d3725af7ee
corbin <corbinchristopher1@gmail.com>
2025-02-26 14:27:48 -0700

    allow api to be tested

--- src/main/java/com/mediavault/service/MediaService.java
+++ src/main/java/com/mediavault/service/MediaService.java
@@ -2,7 +2,9 @@ package com.mediavault.service;
 
 import com.mediavault.entity.Media;
 import com.mediavault.repository.MediaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
+import jakarta.annotation.PostConstruct;
 import java.util.List;
 
 @Service
@@ -23,5 +25,18 @@ public class MediaService {
 
     public Media saveMedia(Media media) {
         return mediaRepository.save(media);
+    }
+    
+    @PostConstruct
+    @Transactional
+    public void initDatabase() {
+        if (mediaRepository.count() == 0) {
+            mediaRepository.saveAll(List.of(
+                new Media(null, "Inception", "/media/movies/inception.mp4", "https://image.tmbd.org/t/p/w500/xyz.jpg", 2010),
+                new Media(null, "The Matrix", "/media/movies/matrix.mp4", "https://image.tmbd.org/t/p/w500/abc.jpg", 1999),
+                new Media(null, "Interstellar", "/media/movies/interstellar.mp4", "https://image.tmbd.org/t/p/w500/pqr.jpg", 2014)
+            ));
+            System.out.println("Bootstrapped test data.");
+        }
     }
 }
