fb8a857f06347ce8b9d028ba5064ad2db0b050ce
garnetraven <corbinchristopher1@gmail.com>
2025-05-02 11:44:04 -0700

    finish up initial system info dto model

--- src/main/java/com/mediavault/dto/SystemInfoDTO.java
+++ src/main/java/com/mediavault/dto/SystemInfoDTO.java
@@ -30,4 +30,93 @@ public class SystemInfoDTO {
         this.totalMemory = totalMemory;
         this.freeMemory = freeMemory;
     }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getServerId() {
+        return serverId;
+    }
+
+    public void setServerid(String serverId) {
+        this.serverId = serverId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getOperatinSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getArchitecture() {
+        return architecture;
+    }
+
+    public void setArchitecture(String architecture) {
+        this.architecture = architecture;
+    }
+
+    public ZonedDataTime getLocalTime() {
+        return localTime;
+    }
+
+    public void setLocalTime(ZonedDataTime localTime) {
+        this.localTime = localTime;
+    }
+
+    public String getStartupPath() {
+        return startupPath;
+    }
+
+    public void setStartupPath(String startupPath) {
+        this.startupPath = startupPath;
+    }
+
+    public String getJavaVersion() {
+        return javaVersion;
+    }
+
+    public void setJavaVersion(String javaVersion) {
+        this.javaVersion = javaVersion;
+    }
+
+    public int getProcessorCount() {
+        return processorCount;
+    }
+
+    public void setProcessorCount(int processorCount) {
+        this.processorCount = processorCount;
+    }
+
+    public long getTotalMemory() {
+        return totalMemory;
+    }
+
+    public void setTotalMemory(long totalMemory) {
+        this.totalMemory = totalMemory;
+    }
+
+    public long getFreeMemory() {
+        return freeMemory;
+    }
+
+    public void setFreeMemory(long freeMemory) {
+        this.freeMemory = freeMemory;
+    }
+
 }
