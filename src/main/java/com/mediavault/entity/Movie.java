5b2ca00ef9947ceb30c01bcbc9515f25e414a1d1
garnetraven <corbinchristopher1@gmail.com>
2025-05-03 15:22:32 -0700

    remove movie class lombok boiler plate

--- src/main/java/com/mediavault/entity/Movie.java
+++ src/main/java/com/mediavault/entity/Movie.java
@@ -2,15 +2,33 @@ package com.mediavault.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import lombok.*;
 
 @Entity
 @DiscriminatorValue("MOVIE")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Movie extends Media {
     private int duration;
     private String director;
+
+    public Movie() {}
+
+    public Movie(int duration, String director) {
+        this.duration = duration;
+        this.director = director;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
 }
