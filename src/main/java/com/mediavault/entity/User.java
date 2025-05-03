fd7ecbd533fb8d3e24582d511db462fd6588f229
garnetraven <corbinchristopher1@gmail.com>
2025-05-03 15:20:08 -0700

    remove user class lombok boiler plate

--- src/main/java/com/mediavault/entity/User.java
+++ src/main/java/com/mediavault/entity/User.java
@@ -1,7 +1,6 @@
 package com.mediavault.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,10 +10,6 @@ import java.util.Collections;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class User implements UserDetails{
 
     @Id
@@ -29,6 +24,47 @@ public class User implements UserDetails{
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    public User() {}
+
+    public User(Long id, String username, String password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
